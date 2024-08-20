b6e1fffa3a9006f6e2ae233596adc59961618778
Daniel Ramirez <50393716+DanielRamirez1023@users.noreply.github.com>
2024-08-20 16:22:41 -0500

    cimplemented onfig urationpfor PWA

--- src/components/botton-navigation.tsx
+++ src/components/botton-navigation.tsx
@@ -14,6 +14,7 @@ export function BottonNavigation() {
         showLabels
         value={value}
         onChange={(event, newValue) => {
+          console.log(event);
           setValue(newValue);
         }}
       >
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "PharmaSolve",
+        short_name: "PharmaSolve",
+        description: "PharmaSolve es una aplicación para la gestión de faltantes en farmacias",
+        theme_color: "#45a9af",
+        background_color: "#ffffff",
+        display: "standalone",
+        display_override: ["window-controls-overlay"],
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
